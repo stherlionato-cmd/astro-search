0d9dcca51da7efe78f69e9530676ce6578c1780d
stherlionato-cmd <stherlionato@gmail.com>
2026-05-03 02:44:23 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -613,7 +613,7 @@ document.querySelectorAll(".btn").forEach(btn=>{
     const x = e.clientX - rect.left - rect.width/2;
     const y = e.clientY - rect.top - rect.height/2;
 
-    btn.style.transform = `translate(${x*0.2}px, ${y*0.2}px)`;
+btn.style.transform = "translate(" + (x*0.2) + "px, " + (y*0.2) + "px)";
   });
 
   btn.addEventListener("mouseleave", ()=>{
